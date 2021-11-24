4067f38d9d6d7c07f269a789ab11015784d5cf44
Dhruv Ukani <dhruvukani28@gmail.com>
2021-11-24 01:53:59 -0500

    Added Product Page and Product Description Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 
 const App = () => {
-  return <Home></Home>;
+  return <Product></Product>;
 };
 
 export default App;
