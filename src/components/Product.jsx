b7ef54fc4dba2f5695e07f8face542d8f33bc715
HAP2Y <phappy1422@gmail.com>
2021-11-23 22:25:15 -0500

    adding Newsletter and Footer

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,80 +1,90 @@
-import { FavoriteBorderOutlined, SearchOutlined, ShoppingBasketOutlined } from "@material-ui/icons";
-import styled from "styled-components"
+import {
+  FavoriteBorderOutlined,
+  SearchOutlined,
+  ShoppingBasketOutlined,
+} from "@material-ui/icons";
+import styled from "styled-components";
+
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+`;
 
 const Container = styled.div`
-    flex: 1;
-    margin: 5px;
-    min-width: 280px;
-    height: 350px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #f5fbfd;
-    position: relative;
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
 `;
 
 const Circle = styled.div`
-    width: 200px;
-    height: 200px;
-    border-radius: 50%;
-    background-color: white;
-    position: absolute;
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
 `;
 
 const Image = styled.img`
-    height: 75%;
-    z-index: 2;
-    
-`;
-const Info = styled.div`
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background-color: #D7EAF0;
-    z-index: 3;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  height: 75%;
+  z-index: 2;
 `;
 
 const Icon = styled.div`
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    background-color: white;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 5px;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 5px;
+  transition: all 0.2s ease-in-out;
 
-    &:hover {
-        background-color: #8FBFD1;
-        transform: scale(1.1);
-        
-    }
+  &:hover {
+    background-color: #d0fef5;
+    transform: scale(1.1);
+  }
 `;
 
-const Product = ({item}) => {
-    return (
-        <Container>
-            <Circle/>
-            <Image src={item.img} />
-            <Info>
-                <Icon>
-                    <ShoppingBasketOutlined/>
-                </Icon>
-                <Icon>
-                    <SearchOutlined/>
-                </Icon>
-                <Icon>
-                    <FavoriteBorderOutlined/>
-                </Icon>
-            </Info>
-        </Container>
-    );
-}
+const Product = ({ item }) => {
+  return (
+    <Container>
+      <Circle />
+      <Image src={item.img} />
+      <Info>
+        <Icon>
+          <ShoppingBasketOutlined />
+        </Icon>
+        <Icon>
+          <SearchOutlined />
+        </Icon>
+        <Icon>
+          <FavoriteBorderOutlined />
+        </Icon>
+      </Info>
+    </Container>
+  );
+};
 
-export default Product
-
+export default Product;
