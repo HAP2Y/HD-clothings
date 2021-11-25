a80fb0482679db3c8769a244b4e535ff1a025a0f
Happy Patel <57473653+HAP2Y@users.noreply.github.com>
2021-11-24 23:14:29 -0500

    Commenting Unused Imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Product from "./pages/Product";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
+//import Home from "./pages/Home";
+//import ProductList from "./pages/ProductList";
+//import Product from "./pages/Product";
+//import Register from "./pages/Register";
+//import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 
 const App = () => {
