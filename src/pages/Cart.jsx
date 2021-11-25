72d667843f142df17abdd7b378497abe7b3f784f
Dhruv Ukani <dhruvukani28@gmail.com>
2021-11-24 22:42:40 -0500

    updated Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components"
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import Newsletter from "../components/Newsletter";
 
 const Container = styled.div``;
 
