206d5ec2e8c5fde14032b07d62e0f605843095c6
HAP2Y <phappy1422@gmail.com>
2021-12-14 14:29:54 -0500

    gitignore env

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,20 @@
 const router = require("express").Router();
-const User = require("../models/User")
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
-router.post("/register", async (req,res) => {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-    });
-    try {
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
-    } catch(err) {
-        res.status(500).json(err);
-    }
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(req.body.password, ""),
+  });
 
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
